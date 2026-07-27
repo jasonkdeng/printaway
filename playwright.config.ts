623b511813e8f3ae9edba0bf536437a0177f7c75
jasonkdeng <jasonkaideng@gmail.com>
2026-07-27 15:48:35 -0400

    feat: config

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     trace: "on-first-retry",
   },
   webServer: {
-    command: "npm run dev",
+    command: "npm run dev:test",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
   },
